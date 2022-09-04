b00f6e3049196ed258e462108faa8607d70b0301
gvspraveen113 <gvspraveen113@gmail.com>
2022-09-04 17:00:45 +0530

    CRUD CloneLI

---
+++ cloneLI/src/main/java/com/dbslinkedin/cloneLI/CloneLiApplication.java
@@ -0,0 +1,13 @@
+package com.dbslinkedin.cloneLI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloneLiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloneLiApplication.class, args);
+	}
+
+}
